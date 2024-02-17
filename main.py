117a1320942ea752f806de79f2a59bce746a7c26
Elena Daurtseva <diosama99@gmail.com>
2024-02-18 02:12:10 +0700

    Data from Api initiation commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+api_key = "812f337180d74507be3a2764dbe69a26"
+url = ("https://newsapi.org/v2/everything?q=tesla&"
+       "from=2024-01-17&sortBy=publishedAt&"
+       "apiKey=812f337180d74507be3a2764dbe69a26")
+
+#Make request
+request = requests.get(url)
+
+#Get a dictionary with data
+content = request.json()
+
+# Access the article titles, description
+for article in content["articles"]:
+       print(article["description"])
+       print(article["title"])
